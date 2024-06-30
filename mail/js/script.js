69b7d697be1932eca2ce83e12be9dfb6e79ff217
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-06-30 22:14:45 +0200

    [UPD] added a cicle for that find out if the value of the prompt is the same of the array

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,5 +1,14 @@
-//dichiaro un'array e assegno una lista di email
+// dichiaro un'array e assegno una lista di email
 let emailList = ['eroe@gmail.com', 'cesare@gmail.com', 'gorilla@gmail.com']
 
-//- dichiaro una varibabile per l'email con valore aquisito da una prompt
+// dichiaro una varibabile per l'email con valore aquisito da una prompt
 let email = prompt('Inserisci la tua email')
+
+// dichiaro una variabile di controllo con valore false
+let allowedEmail = false;
+
+// confronto il valore dell'array con quello aquisito dalla prompt
+for (let i = 0; i < emailList.length; i++) {
+    if (email === emailList[i])
+        allowedEmail = true;
+}
