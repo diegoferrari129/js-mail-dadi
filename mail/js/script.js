c53effc715a2f1621e9e607f13bffb5341c1551e
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-06-30 21:20:29 +0200

    [UPD] created array and prompt to script.js

---
+++ mail/js/script.js
@@ -0,0 +1,5 @@
+//dichiaro un'array e assegno una lista di email
+let emailList = ['eroe@gmail.com', 'cesare@gmail.com', 'gorilla@gmail.com']
+
+//- dichiaro una varibabile per l'email con valore aquisito da una prompt
+let email = prompt('Inserisci la tua email')
