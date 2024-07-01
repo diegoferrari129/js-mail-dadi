9266070bed335b4ec3b08d7424aa3d196e58f151
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-07-01 15:14:18 +0200

    [UPD] created a if section to calculate the winner

--- dice/js/script.js
+++ dice/js/script.js
@@ -2,4 +2,19 @@
 let heroDamage =  Math.floor(Math.random() * 6 + 1);
 
 // dichiaro un'altra variabile con valore random da 1 a 6 per il pc
-let monsterDamage =  Math.floor(Math.random() * 6 + 1); 
+let monsterDamage =  Math.floor(Math.random() * 6 + 1);
+
+// SE il valore assegnato all'utente è maggiore del valore assegnato al pc
+if (heroDamage > monsterDamage) {
+    console.log('The hero won !');
+}
+
+// ALTRIMENTI ?SE il valore assegnato all'utente è minore del valore assegnato al pc
+else if (heroDamage < monsterDamage) {
+    console.log('The hero has been slayed !');
+}
+
+// ALTRIMENTI stampo un'altro messaggio nella console del browser
+else {
+    console.log('Carneficina !');
+}
