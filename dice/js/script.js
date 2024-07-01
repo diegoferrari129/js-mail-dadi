356053877f6ef8967d44704bdaec2f1345a551fd
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-07-01 15:02:28 +0200

    [UPD] created 2 variables with math.random

---
+++ dice/js/script.js
@@ -0,0 +1,5 @@
+// dichiaro una variabile con valore random da 1 a 6 per l'utente
+let heroDamage =  Math.floor(Math.random() * 6 + 1);
+
+// dichiaro un'altra variabile con valore random da 1 a 6 per il pc
+let monsterDamage =  Math.floor(Math.random() * 6 + 1); 
